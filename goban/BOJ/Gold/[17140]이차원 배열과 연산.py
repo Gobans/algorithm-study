082a3a5fa6a39980144dd19b4cdd0163257cd480
Lee Myeonghwan <leemyeonghwan@Leeui-MacBookPro.local>
2022-03-25 22:20:37 +0900

    2022.03.25

--- goban/BOJ/Gold/[17140]이차원 배열과 연산.py
+++ goban/BOJ/Gold/[17140]이차원 배열과 연산.py
@@ -1,40 +1,43 @@
 import sys
-from collections import defaultdict
 r,c,k = map(int,sys.stdin.readline().split())
 A = []
 for _ in range(3):
     A.append(list(map(int,sys.stdin.readline().split())))
-print(A)
+
 #r,c 하나씩 줄여야함 (인덱싱 1 부터 시작이라)
 #정렬: 등장횟수가 커지는 순 -> 등장횟수 같으면 큰 수
 #R연산: 모든 행
 #C연산: 모든 열
+print(A)
 time = 0
 is_ans = False
 while(time < 100):
     row_len = len(A[0])
     col_len = len(A)
     max_len = 0
-    if row_len >= c - 1 and col_len >= r - 1:
-        if A[r-1][c-1] == k:
-            is_ans == True
-            break
 
+    if row_len > c - 1 and col_len > r - 1:
+        if A[r-1][c-1] == k:
+            is_ans = True
+            break
+ 
     #R연산 & C연산
     if row_len <= col_len and row_len < 100:
         #R연산
-        for i in range(row_len):
+        for i in range(col_len):
             #배열 정렬 후 다시 넣기
             dic = {}
-            for j in range(col_len):
+            tmp_list = []
+            for j in range(row_len):
                 if A[i][j] == 0:
-                    break
-                elif dic[A[i][j]]:
+                    pass
+                elif A[i][j] in tmp_list:
                     dic[A[i][j]] += 1
                 else:
-                    dic[A[i][j]] = 0
+                    dic[A[i][j]] = 1
+                    tmp_list.append(A[i][j])
             #딕셔너리 정렬
-            sorted_list = sorted(dic.items())
+            sorted_list = sorted(dic.items(),key=lambda x:(x[1],x[0]))
             #배열 다시 넣기
             A[i].clear()
             for num1,num2 in sorted_list:
@@ -43,43 +46,55 @@ while(time < 100):
             if len(A[i]) > max_len:
                 max_len = len(A[i])
         # 0채워주기
-        for k in range(col_len):
-            while(len(A[k]) < max_len):
-                A[k].append(0)
+        for g in range(col_len):
+            while(len(A[g]) < max_len):
+                A[g].append(0)
+
     elif col_len < 100:
+        row_len = len(A[0])
+        col_len = len(A)
+        max_len = col_len
         for j in range(row_len):
             dic = {}
-            for i in range(row_len):
+            tmp_list = []
+            for i in range(col_len):
                 if A[i][j] == 0:
-                    break
-                elif dic[A[i][j]]:
+                    pass
+                elif A[i][j] in tmp_list:
                     dic[A[i][j]] += 1
                 else:
-                    dic[A[i][j]] = 0
+                    dic[A[i][j]] = 1
+                    tmp_list.append(A[i][j])
             #딕셔너리 정렬
-            sorted_list = sorted(dic.items())
+            sorted_list = sorted(dic.items(),key = lambda x:(x[1],x[0]))
             #배열 다시 넣기
-            if j == 0:
-                for num1,num2 in sorted_list:
-                    A.append([num1])
-                    A.append([num2])
-            else:
-                term = 0
-                for num1,num2 in sorted_list:
-                    A[col_len + term].append(num1)
-                    A[col_len + term +1].append(num2)
-                    term +=2
+
+            if len(sorted_list)*2 > max_len:
+                for _ in range(max_len,len(sorted_list) * 2):
+                    A.append([0])
+                max_len = len(sorted_list)*2
+            term = 0
+            for num1,num2 in sorted_list:
+                while(len(A[term]) <= j):
+                    A[term].append(0)
+                while(len(A[term+1]) <= j):
+                    A[term+1].append(0)
+                A[term][j] = num1
+                A[term +1][j]= num2
+                term +=2
     
         # 0 채워주기
-        for k in range(col_len, len(A)):
-            while(len(A[k]) < row_len):
-                A[k].append(0)
+        for g in range(col_len, len(A)):
+            while(len(A[g]) < row_len):
+                A[g].append(0)
+
+    print(A)
 
     time +=1
     if (time == 100):
         if len(A[0]) >= c - 1 and len(A) >= r - 1:
             if A[r-1][c-1] == k:
-                is_ans == True
+                is_ans = True
                 break
         break
 
